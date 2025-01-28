0a336700b62ebb430843ee6b3304261b24818de8
Ivan <example@gmail.com>
2025-01-28 19:21:15 +0300

    Работаем

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 const number0ffilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalmovieDb = {
-    count: number0ffilms, 
+    count: number0ffilms,
+    movies: {},
+    actors: {},
+    genres: []
+    privat: false
 }
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
