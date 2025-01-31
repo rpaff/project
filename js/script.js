551108c054f9d66fafe42ca48d2295e0d618f56e
Ivan <example@gmail.com>
2025-01-31 17:03:43 +0300

    new lesson

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
-const number0ffilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDb = {
-    count: number0ffilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+"use strict"
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log(Error);
+}
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+// const num = 50;
 
-console.log(personalMovieDb);
+// if (num < 49) {
+//    console.log('Error');
+// } else if (num > 100) {
+//    console.log('Много');
+// } else {
+//    console.log('Ok!');
+// }
+
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+
+const num = '50';
+
+switch (num) {
+    case '49':
+         console.log('Неверно');
+         break;
+    case '100':
+         console.log('Неверно');
+         break;
+    case '50':
+         console.log('В точку');
+         break;
+    default: 
+         console.log('Не в этот раз');
+         break;
+}
+
+
