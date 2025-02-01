ab1ead28aa3a85162ae0786787e85aa089bf3600
Anotheruser <anotheruser@gmail.com>
2025-02-01 03:22:06 +0300

    Логичиские операторы

--- js/script.js
+++ js/script.js
@@ -1,38 +1,15 @@
-"use strict"
+// const hamburger = 5;
+// const fries = null;
 
-if (1) {
-    console.log('Ok!');
-} else {
-    console.log(Error);
-}
-
-// const num = 50;
-
-// if (num < 49) {
-//    console.log('Error');
-// } else if (num > 100) {
-//    console.log('Много');
-// } else {
-//    console.log('Ok!');
+// if (hamburger && fries) {
+//     console.log('Я сыт');
 // }
 
-// (num == 50) ? console.log('Ok!') : console.log('Error');
+const hamburger = 2;
+const fries = 1;
 
-const num = '50';
-
-switch (num) {
-    case '49':
-         console.log('Неверно');
-         break;
-    case '100':
-         console.log('Неверно');
-         break;
-    case '50':
-         console.log('В точку');
-         break;
-    default: 
-         console.log('Не в этот раз');
-         break;
+if (hamburger === 3 && fries) {
+    console.log('Все сыты');
+} else {
+    
 }
-
-
