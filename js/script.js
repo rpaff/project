60a897faed490b382d2cd56f568f0e80e8ed2b42
Ivan <example@gmail.com>
2025-02-02 21:39:54 +0300

    logical operators

--- js/script.js
+++ js/script.js
@@ -5,14 +5,44 @@
 //     console.log('Я сыт');
 // }
 
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'dhfjshjf');
+
 const hamburger = 3;
-const fries = 1;
-const cola = 1;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
-console.log(hamburger === 3 && cola && fries);
 
-if (hamburger === 3 && cola === 1 && fries) {
-    console.log('Все сыты');
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('Все довольны!');
 } else {
     console.log('Мы уходим');
 }
+
+console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets); 
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log (1 && 2 && 3);
+
+console.log (!1 && 2 || !3);
+
+console.log (25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
+
