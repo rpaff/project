b6d089ec8cd844f801b910ebf4384220ecfe9618
Ivan <example@gmail.com>
2025-02-02 21:46:46 +0300

    While

--- js/script.js
+++ js/script.js
@@ -1,48 +1,13 @@
-// const hamburger = 5;
-// const fries = null;
+"use strict";
 
-// if (hamburger && fries) {
-//     console.log('Я сыт');
-// }
+let num = 50;
 
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'dhfjshjf');
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Все довольны!');
-} else {
-    console.log('Мы уходим');
+while (num <= 55) {
+    console.log(num);
+    num++;
 }
 
-console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets); 
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-console.log(!0);
-
-console.log(NaN || 2 || undefined);
-
-console.log(NaN && 2 && undefined);
-
-console.log (1 && 2 && 3);
-
-console.log (!1 && 2 || !3);
-
-console.log (25 || null && !3);
-
-console.log(NaN || null || !3 || undefined || 5);
-
-console.log(NaN || null && !3 && undefined || 5);
-
-console.log(5 === 5 && 3 > 1 || 5);
-
+do {
+    console.log(num);
+    num++;
+}
