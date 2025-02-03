90e06e33e46a0e8226a6aa6d4c4439a02447dcdf
Ivan <example@gmail.com>
2025-02-03 20:12:41 +0300

    вложенные циклы

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ for (let i = 0; i < 3; i++) {
     }
 
 }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j =0; j < i; j++) {
+        result +="*";
+    }
+
+    result +='\n';
+}
+
+
+console.log(result);
