b92cb64c56a183704363ced4fd4f1e43e58634e9
Ivan <example@gmail.com>
2025-02-03 19:32:33 +0300

    while inside while

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-"use strict";
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
 
-let num = 50;
-
-while (num <= 55) {
-    console.log(num);
-    num++;
-}
-
-do {
-    console.log(num);
-    num++;
 }
