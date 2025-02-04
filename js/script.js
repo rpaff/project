78209c21e78277c2b79eb805c582d34f25808da1
Ivan <example@gmail.com>
2025-02-04 19:12:33 +0300

    продолжаем тему условий и циклов

--- js/script.js
+++ js/script.js
@@ -7,4 +7,28 @@
 возвращаем пользователя к вопросам опять. (К любой строке можно обратить как str.length - и получить её длину)
 
 3) При помощи условия проверить personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов" если от 10
+"Просмотрено довольно мало фильмов" если от 10 */
+
+'use strict';
+
+const number0ffilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: number0ffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+    if ()
+
+    personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
