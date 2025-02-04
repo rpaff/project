be4a2582deeb31807b59f4b001b8cbce10d7aa0a
Ivan <example@gmail.com>
2025-02-04 14:03:52 +0300

    some other cycles

--- js/script.js
+++ js/script.js
@@ -27,3 +27,12 @@ for (let i = 1; i < length; i++) {
 
 
 console.log(result);
+
+for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+        for (let j = 0; j < 3; j++) {
+            console.log(j);
+    }
+    }
