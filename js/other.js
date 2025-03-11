9278cc0fb847ea7eca3069fc1a2a4b275ca901c8
Anotheruser <anotheruser@gmail.com>
2025-03-11 10:44:30 +0300

    massives

--- js/other.js
+++ js/other.js
@@ -17,5 +17,10 @@ const options = {
 // console.log(options);
 
 for (let key in options) {
-    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    }
+  
 }
