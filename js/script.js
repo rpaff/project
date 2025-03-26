63fd74a1b827cf73e4e3dba6a5a07db1781fad78
Anotheruser <anotheruser@gmail.com>
2025-03-26 17:51:41 +0300

    getelement

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
 "use strict";
 
-// To String
+const box = document.getElementById('box');
 
-// 1)
-console.log(typeof(String(null)));
+console.log(box);
 
-console.log(typeof(String(4)));
+const btns = document.getElementsByClassName('button');
 
-// 2)
-console.log(typeof(null + ''));
+console.log(btns[1]);
 
-const num = 5;
-
-console.log("https//vk.com/catalog/" + num);
-
-const fontSize = 26 + 'px';
-
-// To Number 
-
-// 1)
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(+'5'));
-
-
-
+const circles = document.getElementsByClassName('circle');
 
 
